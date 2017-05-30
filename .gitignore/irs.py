23160ee7715f9b7e208af0c89a7a761ee26bd069
Prakshi <yprakshi@gmail.com>
2017-05-30 17:06:01 +0530

    This is the main script

---
+++ .gitignore/irs.py
@@ -0,0 +1,14 @@
+from sklearn import svm
+from sklearn import datasets
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score #works
+
+clf = svm.SVC()
+iris = datasets.load_iris()
+validation_size = 0.30
+X, Y = iris.data, iris.target
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=validation_size)
+clf.fit(X_train, Y_train)
+predictions = clf.predict(X_test)
+print(accuracy_score(Y_test, predictions))
